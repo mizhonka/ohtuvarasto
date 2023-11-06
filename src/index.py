24db1351508b7419f992ceb45bb083ad2e75e9e8
Miranda Honkanen <miranda.honkanen@helsinki.fi>
2023-11-06 18:28:25 +0200

    Fixed pylint

--- src/index.py
+++ src/index.py
@@ -5,6 +5,10 @@ def main():
     mehua = Varasto(100.0)
     olutta = Varasto(100.0, 20.2)
 
+    print("Luonnin jälkeen:")
+    print(f"Mehuvarasto: {mehua}")
+    print(f"Olutvarasto: {olutta}")
+
 
 if __name__ == "__main__":
     main()
